19a98da58b2851196df26a4d9770ec62349d0853
luciu <luciu@M15-I5W303>
2025-04-20 21:33:47 +0700

    Добавьте файлы проекта.

---
+++ lab10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        
+        File.Create(@".\input.txt").Close();
+        File.Create(@".\output.txt").Close();
+
+        FileInfo fi = new FileInfo(@".\input.txt");
+        FileInfo fi_1 = new FileInfo(@".\output.txt");
+        int max = -101;
+        int min = 101;
+
+        using (StreamWriter SW = new StreamWriter(@".\input.txt"))      //fi.AppendText())
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                int numb = rand.Next(-100, 101);
+                SW.WriteLine($"{numb}");
+
+                if (numb > max)
+                { max = numb; }
+                if (numb < min)
+                { min = numb; }
+            }
+        }
+
+        
+
+        string[] text = File.ReadAllLines(fi.FullName);
+
+        using (StreamWriter SW = new StreamWriter(@".\output.txt")) //fi_1.AppendText())
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                if (int.Parse(text[i]) % 2 == 0)
+                { text[i] = $"{Math.Abs(max - min)}"; }
+                SW.WriteLine(text[i]);
+            }
+        }
+
+
+
+    }
+
+}
